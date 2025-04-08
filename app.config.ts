2dbf8aa6bab40ed4ffadc383bc7ef9ab3c64e9e6
laoqin2024 <153482085+laoqin2024@users.noreply.github.com>
2025-04-08 13:52:03 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
   title: 'Sink',
   email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
+  github: 'https://github.com/laoqin2024/Sink',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'Sink是一个简单的/快速的/安全的链接缩短器，100%运行在 Cloudflare 上。',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
